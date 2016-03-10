527986dd42071a75c7635f87b3bf56fee5044214
Thierry Rascle <thierr26@free.fr>
2016-03-10 23:33:25 +0100

    Add new test ((C) section and function body)

---
+++ test/expected_test_output/test8.m
@@ -0,0 +1,8 @@
+function test8
+    1;
+
+end
+
+% Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
+% MIT license. Please refer to the LICENSE file.
+% Author: Thierry Rascle <thierr26@free.fr>
---
+++ test/test_input/test8.m
@@ -0,0 +1,9 @@
+## Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
+## MIT license. Please refer to the LICENSE file.
+## Author: Thierry Rascle <thierr26@free.fr>
+
+function test8
+
+    1;
+
+endfunction
