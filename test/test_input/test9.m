fa8b6d43a7d33e9135282a313af16d17b4ad75ca
Thierry Rascle <thierr26@free.fr>
2016-03-12 00:11:48 +0100

    Fix spacing issue in function signatures

---
+++ test/test_input/test9.m
@@ -0,0 +1,51 @@
+## Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
+## MIT license. Please refer to the LICENSE file.
+
+## -*- texinfo -*-
+## @deftypefn  {Function File} test1
+##
+## Useless function.  Written only to test script octaveh2matlabh. Returns a
+## boolean value.
+##
+## @seealso{test2}
+##
+## @end deftypefn
+
+## Author: Thierry Rascle <thierr26@free.fr>
+
+function ret = test9(x, ...
+    y, ...
+    z)
+
+    try
+
+        ret = true;
+
+        for k = 1 : 3
+            ret = ~ret;
+        endfor
+
+        while ~ret
+            ret = ~ret;
+        endwhile
+
+        if rand > 0.5
+            n = 1;
+        else
+            n = 2;
+        endif
+
+        switch n
+            # Bla.
+            case 1
+                ret = ~ret;
+        endswitch
+        fs = '\';
+        fprintf('foo\nbar');
+        fprintf('foo\tbar');
+
+    catch
+        ret = false;
+    end_try_catch
+
+endfunction
